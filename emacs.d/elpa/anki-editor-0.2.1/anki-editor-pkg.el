551a9f0a16efb3e22f4d760765f9c085e92a82f1
Ben Miller <benmiller179@gmail.com>
2018-05-09 10:03:22 +0900

    add elpa to emacs.d

---
+++ emacs.d/elpa/anki-editor-0.2.1/anki-editor-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "anki-editor" "0.2.1" "Make Anki Cards in Org-mode" '((emacs "25")) :authors '(("Louie Tan")) :maintainer '("Louie Tan") :url "https://github.com/louietan/anki-editor")
